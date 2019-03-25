95677adffdd481488b13525fae01feeb33371888
EGOIST <0x142857@gmail.com>
2019-03-26 01:28:58 +0800

    fix: set user agent

--- src/index.js
+++ src/index.js
@@ -14,6 +14,7 @@ async function getHTML(
   onFetch && onFetch(url)
   const page = await browser.newPage()
   await page.setRequestInterception(true)
+  await page.setUserAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36 Prerender')
   page.on('request', interceptedRequest => {
     const type = interceptedRequest.resourceType()
     const resourceURL = interceptedRequest.url()
