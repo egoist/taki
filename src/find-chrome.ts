6873117fb15c1d7463c0ee3d7875c09e0567cb48
Kevin Hazy <0x142857@gmail.com>
2020-10-16 15:58:07 +0800

    fix: respect CHROME_PATH from environment variable

--- src/find-chrome.ts
+++ src/find-chrome.ts
@@ -1,8 +1,11 @@
 import fs from 'fs'
 
+const { CHROME_PATH } = process.env
+
 const paths: string[] =
   process.platform === 'darwin'
     ? [
+        CHROME_PATH,
         '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
         '/Applications/Google Chrome Canary.app/Contents/MacOS/Google Chrome Canary',
         '/Applications/Chromium.app/Contents/MacOS/Chromium',
@@ -13,6 +16,7 @@ const paths: string[] =
       ]
     : process.platform === 'win32'
     ? [
+        CHROME_PATH,
         process.env.LOCALAPPDATA + '/Google/Chrome/Application/chrome.exe',
         process.env.PROGRAMFILES + '/Google/Chrome/Application/chrome.exe',
         process.env['PROGRAMFILES(X86)'] +
@@ -22,6 +26,7 @@ const paths: string[] =
         process.env['PROGRAMFILES(X86)'] + '/Chromium/Application/chrome.exe',
       ]
     : [
+        CHROME_PATH,
         '/usr/bin/google-chrome-stable',
         '/usr/bin/google-chrome',
         '/usr/bin/chromium',
@@ -31,7 +36,7 @@ const paths: string[] =
 
 export function findChrome(): string {
   for (const p of paths) {
-    if (fs.existsSync(p)) {
+    if (p && fs.existsSync(p)) {
       return p
     }
   }
