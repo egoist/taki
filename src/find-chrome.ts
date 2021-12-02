d6c7b2fda5c995539f37379af5aa99dc7b123dfc
Anthony Fu <anthonyfu117@hotmail.com>
2021-12-02 21:14:58 +0800

    feat: use locally installed `chromium` if preset (#6)
    Co-authored-by: EGOIST <0x142857@gmail.com>

--- src/find-chrome.ts
+++ src/find-chrome.ts
@@ -2,10 +2,9 @@ import fs from 'fs'
 
 const { CHROME_PATH } = process.env
 
-const paths: (string | undefined)[] =
+const paths_os: (string | undefined)[] =
   process.platform === 'darwin'
     ? [
-        CHROME_PATH,
         '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
         '/Applications/Google Chrome Canary.app/Contents/MacOS/Google Chrome Canary',
         '/Applications/Chromium.app/Contents/MacOS/Chromium',
@@ -16,7 +15,6 @@ const paths: (string | undefined)[] =
       ]
     : process.platform === 'win32'
     ? [
-        CHROME_PATH,
         process.env.LOCALAPPDATA + '/Google/Chrome/Application/chrome.exe',
         process.env.PROGRAMFILES + '/Google/Chrome/Application/chrome.exe',
         process.env['PROGRAMFILES(X86)'] +
@@ -26,7 +24,6 @@ const paths: (string | undefined)[] =
         process.env['PROGRAMFILES(X86)'] + '/Chromium/Application/chrome.exe',
       ]
     : [
-        CHROME_PATH,
         '/usr/bin/google-chrome-stable',
         '/usr/bin/google-chrome',
         '/usr/bin/chromium',
@@ -34,7 +31,20 @@ const paths: (string | undefined)[] =
         '/snap/bin/chromium',
       ]
 
+function getLocalChromiumPath(): string | undefined {
+  try {
+    const { path } = require('chromium')
+    return path
+  } catch {}
+}
+
 export function findChrome(): string {
+  const paths = [
+    getLocalChromiumPath(),
+    CHROME_PATH,
+    ...paths_os,
+  ]
+  
   for (const p of paths) {
     if (p && fs.existsSync(p)) {
       return p
